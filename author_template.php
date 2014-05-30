df0c659fff16e023b003f83039ffc72f7d8b50e5
Lensgeek <Alexedwards@f2s.com>
2014-05-30 14:44:19 +0100

    Final Commit before Live

--- author_template.php
+++ author_template.php
@@ -15,56 +15,100 @@ foreach($result_array as $key => $value) {
 */
 
 ?>
+
+<div class="row">
+      
+</div> 
+<div class="row header-logo">
+  	<div class="medium-12">
+		<div class=" medium-4 columns text-left">
+	       <ul class="no-bullet logo">
+          <li><a href="author/rachel-appleby" title="Rachel Appleby">Rachel Appleby</a></li>
+          <li><a href="author/ioana-bultoc" title="Ioana Bultoc">Ioana Bultoc</a></li>
+          <li><a href="author/heidi-culverwell" title="Heidi Culverwell">Heidi Culverwell</a></li>
+          <li><a href="author/alex-edwards" title="Alex Edwards">Alex Edwards</a></li>
+          <li><a href="author/theo-ellison" title="Theo Ellison">Theo Ellison</a></li>
+          <li><a href="author/kate-green" title="Kate Green">Kate Green</a></li>
+          <li><a href="author/lee-hassall" title="Lee Hassall">Lee Hassall</a></li>
+          <li><a href="author/talha-hassan" title="Talha Hassan">Talha Hassan</a></li>
+          <li><a href="author/joseph-kesisoglou" title="Joseph Kesisoglou">Joseph Kesisoglou</a></li>
+          <li><a href="author/alice-martin-smith" title="Alice Martin-Smith">Alice Martin Smith</a></li>
+        </ul>
+	   	</div>
+	   	<div class="medium-4 columns text-center logo-header-author">
+	          <a href=""><img src="img/img19_circle2.gif" /></a>
+	      </div>
+	    <div class="medium-4 columns text-right">
+	       <ul class="no-bullet logo">
+          <li><a href="author/alex-mason" title="Alex Mason">Alex Mason</a></li>
+          <li><a href="author/thaia-merriman" title="Tania Merriman">Tania Merriman</a></li>
+          <li><a href="author/sophie-moet" title="Sophie Moet">Sophie Moet</a></li>
+          <li><a href="author/melissa-santos" title="Melissa Santos">Melissa Santos</a></li>
+          <li><a href="author/melissa-stapleton" title="Melissa Stapleton">Melissa Stapleton</a></li>
+          <li><a href="author/lauren-thatcher" title="Lauren Thatcher">Lauren Thatcher</a></li>
+          <li><a href="author/christopher-trafford" title="Christopher Trafford">Christopher Trafford</a></li>
+          <li><a href="author/caroline-walker" title="Caroline Walker">Caroline Walker</a></li>
+          <li><a href="author/hollie-woodward" title="Hollie Woodward">Hollie Woodward</a></li>
+        </ul>
+		    </div>
+		</div>
+    </div> 
+</div>
+    <div class="row panel">
+
 <div class="large-12 columns" />
 	<div class="large-10 columns large-centered">
 		<img src="<?= $result_array['content_mainimage']; ?>" />
+		<div class="row">
+			<div class="large-7 columns">
+				<h2><?= $result_array['content_title']; ?></h2>
+				<h4 class="subheader">by <?= $result_array['author_first']." ".$result_array['author_last']; ?></h4>
+			</div>
+			<div class="large-3 columns social-links">
+				<?php
+
+					if (isset($result_array['contact_website'])) {
+						?>
+						<a href="<?= $result_array['contact_website']; ?>" target="_blank">
+							<i class="fi-link"></i>
+						</a>
+						<?php
+
+					};
+
+					if (isset($result_array['contact_twit'])) {
+						?>
+						<a href="http://twitter.com/<?= $result_array['contact_twit']; ?>" target="_blank">
+							<i class="fi-social-twitter"></i>
+						</a>
+						<?php
+
+					};
+
+					if (isset($result_array['contact_insta'])) {
+						?>
+						<a href="http://instagram.com/<?= $result_array['contact_insta']; ?>" target="_blank">
+							<i class="fi-social-instagram"></i>
+						</a>
+						<?php
+
+					}; ?>
+			</div>
+		</div>
+		<div class="columns main-text">
+			<p>
+				<?= $result_array['content_maintext']; ?>
+			</p>
+		</div>
 	</div>
-		<div class="large-8 columns">
-		<h2><?= $result_array['content_title']; ?>
-			<h4 class="subheader">by <?= $result_array['author_first']." ".$result_array['author_last']; ?></h4>
-		</div>
-		<div class="large-4 columns social-links">
-			<?php
-
-				if (isset($result_array['contact_website'])) {
-					?>
-					<a href="<?= $result_array['contact_website']; ?>" target="_blank">
-						<i class="fi-link"></i>
-					</a>
-					<?php
-
-				};
-
-				if (isset($result_array['contact_twit'])) {
-					?>
-					<a href="http://twitter.com/<?= $result_array['contact_twit']; ?>" target="_blank">
-						<i class="fi-social-twitter"></i>
-					</a>
-					<?php
-
-				};
-
-				if (isset($result_array['contact_insta'])) {
-					?>
-					<a href="http://instagram.com/<?= $result_array['contact_insta']; ?>" target="_blank">
-						<i class="fi-social-instagram"></i>
-					</a>
-					<?php
-
-				}; ?>
-		</div>
-
-	<div class="large-12 columns text-center">
-		<p>
-			<?= $result_array['content_maintext']; ?>
-		
+<div class="large-12 columns text-center"><p>
 		<?php
 
 		if (isset($result_array['content_mainwork'])) {
 					
 					print_r($result_array['content_mainwork']);
 
-			}; 
+			};  ?> </p><p class="content_further"><?php
 
 		if (isset($result_array['content_further'])) {
 					
@@ -73,22 +117,36 @@ foreach($result_array as $key => $value) {
 			}; 
 		?>
 		</p>
-		<p> To follow up on the work please contact the artist via : <br />
+		<p> 
 			<?php
-			if (isset($result_array['contact_email'])) {
-					
-					echo "<i class='fi-mail'></i>";
+				$contact_via = 0;
+
+				if (isset($result_array['contact_email'])) {
+						$contact_via += 1;
+				};
+
+				if (isset($result_array['contact_phone'])) {
+						$contact_via += 2;
+				}; 
+
+				if ($contact_via == "1") {
+					echo "To follow up on the work please contact the artist via email @ <br />";
+					echo "<i class='fi-mail'></i> ";
 					print_r($result_array['contact_email']);
 					echo "<br />";
-
-			};
-
-			if (isset($result_array['contact_phone'])) {
-					
-					echo "<i class='fi-telephone'></i>";
+				}elseif ($contact_via == "2") {
+					echo "To contact the artist about the work, pelase phone @ <br />";
+					echo "<i class='fi-telephone'></i> ";
 					print_r($result_array['contact_phone']);
-
-			}; 
+					echo "<br />";
+				}elseif ($contact_via == "3") {
+					echo "To contact the artist about the work, pelase phone or email: <br />";
+					echo "<i class='fi-telephone'></i> ";
+					print_r($result_array['contact_phone']);
+					echo "<br />";
+					echo "<i class='fi-mail'></i> ";
+					print_r($result_array['contact_email']);
+				};
 
 			?>
 		</p>
--- index.php
+++ index.php
@@ -10,19 +10,13 @@
     <link rel="icon" type="image/gif" href="fav/favicon_64.gif">
   </head>
   <body>
-    <div class="row">
-      <div class="large-12 columns text-center logo-header">
-          <a href=""><img src="img/img19_circle2.gif" /></a>
-      </div>
-    </div> 
-    <div class="row panel">
-
+    
 <?php 
 
   include_once "myQLserver.php";
   $data = $_SERVER['QUERY_STRING']; 
   parse_str($data, $data_array); // Breaks the URL into it's componants after ? & Returns as Array [cancelling out the & split]
-  if (isset($data_array['author'])) { 
+  if (isset($data_array['author'])) {  //If URL contains an Author's name
     $query_author = "SELECT * FROM author WHERE author_id = '". $data_array['author'] ."'";
     $result_array = [];
     if ($result = mysqli_query($link, $query_author)) {
@@ -34,35 +28,42 @@
     mysql_close($link);
   }else{
     ?>
+    <div class="row">
+      <div class="large-4 columns text-center large-centered logo-header">
+          <a href=""><img src="img/img19_circle2.gif" /></a>
+      </div>
+    </div> 
+    <div class="row panel">
+
         <div class="large-10 large-centered columns text-center">
         <p>Photography, considered a modern art form, is no longer bound by the paper’s edge; the exhibition will evidence this as it showcases a variety of forms which photography can take in contemporary practice. The featured artists use photography as a medium to raise awareness on important social demographics and engage with current debates such as the relation between photography, history, memory and space.</p>
         <p><i class="logo">IMG19</i> Features New Works By :</p>
       </div>    
       <div class=" medium-6 large-6 columns text-center">
         <ul class="no-bullet logo">
-          <li>Rachel Appleby</li>
-          <li>Ioana Bultoc</li>
-          <li>Heidi Culverwell</li>
-          <li>Alex Edwards</li>
-          <li>Theo Ellison</li>
-          <li>Kate Green</li>
-          <li>Lee Hassall</li>
-          <li>Talha Hassan</li>
-          <li>Joseph Kesisoglou</li>
-          <li>Alice Martin Smith</li>
+          <li><a href="author/rachel-appleby" title="Rachel Appleby">Rachel Appleby</a></li>
+          <li><a href="author/ioana-bultoc" title="Ioana Bultoc">Ioana Bultoc</a></li>
+          <li><a href="author/heidi-culverwell" title="Heidi Culverwell">Heidi Culverwell</a></li>
+          <li><a href="author/alex-edwards" title="Alex Edwards">Alex Edwards</a></li>
+          <li><a href="author/theo-ellison" title="Theo Ellison">Theo Ellison</a></li>
+          <li><a href="author/kate-green" title="Kate Green">Kate Green</a></li>
+          <li><a href="author/lee-hassall" title="Lee Hassall">Lee Hassall</a></li>
+          <li><a href="author/talha-hassan" title="Talha Hassan">Talha Hassan</a></li>
+          <li><a href="author/joseph-kesisoglou" title="Joseph Kesisoglou">Joseph Kesisoglou</a></li>
+          <li><a href="author/alice-martin-smith" title="Alice Martin-Smith">Alice Martin Smith</a></li>
         </ul>
       </div>
       <div class="medium-6 large-6 columns text-center">
         <ul class="no-bullet logo">
-          <li>Alex Mason</li>
-          <li>Tania Merriman</li>
-          <li>Sophie Moet</li>
-          <li>Melissa Santos</li>
-          <li>Melissa Stapleton</li>
-          <li>Lauren Thatcher</li>
-          <li>Christopher Trafford</li>
-          <li>Caroline Walker</li>
-          <li>Hollie Woodward</li>
+          <li><a href="author/alex-mason" title="Alex Mason">Alex Mason</a></li>
+          <li><a href="author/thaia-merriman" title="Tania Merriman">Tania Merriman</a></li>
+          <li><a href="author/sophie-moet" title="Sophie Moet">Sophie Moet</a></li>
+          <li><a href="author/melissa-santos" title="Melissa Santos">Melissa Santos</a></li>
+          <li><a href="author/melissa-stapleton" title="Melissa Stapleton">Melissa Stapleton</a></li>
+          <li><a href="author/lauren-thatcher" title="Lauren Thatcher">Lauren Thatcher</a></li>
+          <li><a href="author/christopher-trafford" title="Christopher Trafford">Christopher Trafford</a></li>
+          <li><a href="author/caroline-walker" title="Caroline Walker">Caroline Walker</a></li>
+          <li><a href="author/hollie-woodward" title="Hollie Woodward">Hollie Woodward</a></li>
         </ul>
       </div>
     </div>
@@ -82,11 +83,16 @@
           ?>
         </div>
         <div class="large-4 columns">
-          <p>Download a file here <i class="fi-page-export-pdf"></i></p>
-          <p>Launch into an external webpage <i class="fi-link"></i></p>
-          <p><i class="fi-marker"></i> Logo for Gallery Map location</p>
-          <p><i class="fi-magnifying-glass"></i> Get more information</p>
-          <p><i class="fi-calendar"></i> Schedule &amp; opening times</p>
+          <p>Navigate <i class="logo">img19</i></p>
+          <p>View the Official Press Release <a href="assets/IMG19_PressRelease.pdf" download="IMG19 Press Release.pdf"><i class="fi-page-export-pdf"></i></a></p>
+          <p><i class="fi-marker"></i> Find people's work on our Map</p>
+          <p><i class="fi-calendar"></i> Schedule &amp; opening times:</p>
+            <ul class="no-bullet">
+              <li>Opening Night: Friday 30th. 6pm - 9pm</li>
+              <li>Saturday 31/05 - Thursday 05/06</li>
+              <li>10am - 5pm (4pm Sunday)</li>
+
+            </ul>
         </div>
         <div class="instagram-feed large-4 columns">
           <p><i class="fi-social-instagram"></i> Follow Our Instagram</p>
